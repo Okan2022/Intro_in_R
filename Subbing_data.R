faae6f7093ff70552ba6125ce7c7b4826589ef38
Okan2022 <o.sarioglu@gmx.de>
2023-08-22 19:32:35 +0200

    updated file

---
+++ Subbing_data.R
@@ -0,0 +1,15 @@
+library(haven)
+library(magrittr)
+library(dplyr)
+
+ess <- read.csv("data/ESS10.csv")
+
+input <- names(ess[1:20])
+
+
+ESS10 <- ess %>% 
+  select(input, cntry, agea, trstsci, getnvc19,
+         eisced, gndr, lrscale)
+
+write.csv(ESS10, "data/ESS10_sub.csv", row.names = T)
+
