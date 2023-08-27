5eefc219a3828fafc87a46133309fbc9c05cb92b
Okan2022 <o.sarioglu@gmx.de>
2023-08-27 16:40:03 +0200

    updated files

--- Subbing_data.R
+++ Subbing_data.R
@@ -9,7 +9,7 @@ input <- names(ess[1:20])
 
 ESS10 <- ess %>% 
   select(input, cntry, agea, trstsci, getnvc19,
-         eisced, gndr, lrscale)
+         eisced, gndr, lrscale, eisced, dscrgrp, cttresa)
 
 write.csv(ESS10, "data/ESS10_sub.csv", row.names = T)
 
